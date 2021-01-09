8ee4a6c09f14321be3e532095dd9e8d0b2f9769c
wwpcwzf <wwpcwzf@163.com>
2021-01-09 16:35:31 +0800

    注释

--- app/mesh_light/app/wwpc_main_loop.c
+++ app/mesh_light/app/wwpc_main_loop.c
@@ -6,14 +6,16 @@
         |
 ******************************************************************************************/
 #include "wwpc_main_loop.h"
-
+#include "wwpc_config.h"
 
 
 
 void LUTEC_MainLoop(void)
 {
 
-
+#if LUTEC_DEBUG_UART_ENABLE
+        
+#endif //LUTEC_DEBUG_UART_ENABLE
 
 }
 
